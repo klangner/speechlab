44147cea99633321afa3d33eeef25a7513e8a308
klangner <klangner@gmail.com>
2015-05-10 15:55:46 +0200

    Refactored audio processing to separate class and thread

--- app/src/main/java/com/klangner/speechlab/MainActivity.java
+++ app/src/main/java/com/klangner/speechlab/MainActivity.java
@@ -9,38 +9,19 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.klangner.speechlab.signal.SignalProcessor;
-
 
 public class MainActivity extends ActionBarActivity {
 
     private static final String TAG = "MainActivity";
-    private static final int RECORDER_SAMPLERATE = 44100;
-    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
-    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
-    private static final int SAMPLE_SIZE = 2048;
 
     private AudioRecord recorder = null;
-    private Thread recordingThread = null;
-    private boolean isRecording = false;
+    private SignalPublisher signalPublisher = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.v(TAG, "onCreate");
-    }
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-        initAudioCapture();
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStart();
-        releaseAudioCapture();
     }
 
     @Override
@@ -51,25 +32,19 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onPause(){
-        super.onPause();
         stopRecording();
+        super.onPause();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -77,46 +52,19 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void initAudioCapture() {
-        Log.v(TAG, "Init audio");
-        int bufferSize = AudioRecord.getMinBufferSize(RECORDER_SAMPLERATE, RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING);
-        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, RECORDER_SAMPLERATE,
-                RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING, bufferSize);
-    }
-
-    private void releaseAudioCapture() {
-        Log.v(TAG, "Destroy audio");
-        if(recorder != null) {
-            recorder.release();
-            recorder = null;
-        }
-    }
-
     private void startRecording() {
-        Log.v(TAG, "Start recording");
-        if(recorder != null) {
-            recorder.startRecording();
-            recordingThread = new Thread(new Runnable() {
-                public void run() {
-                    processAudio();
-                }
-            }, "AudioRecorder Thread");
-            isRecording = true;
-            recordingThread.start();
-        }
+        signalPublisher = new SignalPublisher();
+        signalPublisher.addPowerSubscriber(new SignalPublisher.PowerSubscriber() {
+            public void onChanged(double power) {
+                Log.v(TAG, "Power: " + power);
+            }
+        });
+        Thread thread = new Thread(signalPublisher);
+        thread.start();
     }
 
     private void stopRecording(){
         Log.v(TAG, "Stop recording");
-        isRecording = false;
-    }
-
-    private void processAudio() {
-        short[] sample = new short[SAMPLE_SIZE];
-        while (isRecording) {
-            recorder.read(sample, 0, SAMPLE_SIZE);
-            double power = SignalProcessor.logPower(sample);
-            Log.v(TAG, "Power: " + power);
-        }
+        signalPublisher.stop();
     }
 }
